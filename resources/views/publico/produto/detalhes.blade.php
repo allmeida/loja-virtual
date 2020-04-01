272d43911bb75dedd66565a3abc755a08b5b43e4
Allmeida <allmeida.fran@gmail.com>
2020-04-01 18:38:09 -0400

    Ajuste Detalhes do Produto

--- resources/views/publico/produto/detalhes.blade.php
+++ resources/views/publico/produto/detalhes.blade.php
@@ -2,12 +2,14 @@
 
 @section('conteudo')
     <div class="row mt-2">
-        <div class="col-md-3 text-center">
+        <div class="card-body">
+        <div class="col-md-3">
             <img src="{{ asset('/imagens/' . $produto->imagem) }}" class="card-img-top mb-2" alt="...">
 
             {!! Form::open(['url' => route('publico.carrinho.store')]) !!}
                 {!! Form::hidden('produto', $produto->id) !!}
-                {!! Form::submit('Adicionar ao Carrinho', ['class' => 'btn btn-primary']) !!}
+                {!! Form::number('quantidade', 1, ['class' => 'form-control text-center mb-1']) !!}
+                {!! Form::submit('Adicionar ao Carrinho', ['class' => 'btn btn-primary btn-block']) !!}
             {!! Form::close() !!}
         </div>
         <div class="col-md-9">
@@ -16,6 +18,7 @@
 
             <h5 class="text-primary mt-3">Descrição</h5>
             <p>{{ $produto->descricao }}</p>
+        </div>
         </div>
     </div>
 @endsection
